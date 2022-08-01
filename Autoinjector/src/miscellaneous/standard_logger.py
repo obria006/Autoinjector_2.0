8b83704ac97f5a281e7e082e8f3cad6e19f60e00
Jacob O'Brien <obria006@umn.edu>
2022-08-01 11:36:12 +0200

    Init commit for class to handle logging

---
+++ Autoinjector/src/miscellaneous/standard_logger.py
@@ -0,0 +1,39 @@
+''' Class for handling logging '''
+import logging
+from datetime import datetime
+from src.miscellaneous import validify as val
+
+class StandardLogger(logging.Logger):
+    '''
+    Defines a standard logger that can be used for most in autoinjector GUI.
+    Streams all logger levels to the console, and streams warnings and above
+    to a file. File is located at logs/YYYYMMDD.log
+    '''
+
+    def __init__(self, name:str):
+        '''
+        Creates logging.Logger instance with the passed name
+
+        Arguments:
+            name (str): Name of Logger (usually passed as __name__ of the file
+                the logger is located in)
+        
+        Returns:
+            instance of logging.Logger
+        '''
+        super().__init__(name=name)
+        if val.is_of_types(name, [str]) is False:
+            raise TypeError('name of logger must be a string')
+        # self = logging.getLogger(name)
+        s_handler = logging.StreamHandler()
+        s_handler.setLevel(logging.DEBUG)
+        s_formatter = logging.Formatter('%(levelname)s - %(name)s: %(message)s')
+        s_handler.setFormatter(s_formatter)
+        self.addHandler(s_handler)
+        now = datetime.now().strftime("%Y%m%d")
+        log_path = f"logs/{now}.log"
+        f_handler = logging.FileHandler(log_path)
+        f_handler.setLevel(logging.WARNING)
+        f_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+        f_handler.setFormatter(f_formatter)
+        self.addHandler(f_handler)
